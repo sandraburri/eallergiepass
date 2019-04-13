be072a514ed142561f2df037320b39dbbc3253b8
Sandra Burri <sandra.burri@students.bfh.ch>
2019-04-13 17:21:41 +0200

    Added a nuxt.js single page application, copy assets to public folder using mix.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,13 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.js('resources/js/app.js', 'public/js')
+mix.sass('resources/sass/app.scss', 'public/css');
 
-mix.react('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+
+mix.copy('spa/dist/.nojekyll', 'public/.nojekyll')
+mix.copy('spa/dist/200.html', 'public/200.html')
+mix.copy('spa/dist/icon.png', 'public/icon.png')
+mix.copy('spa/dist/sw.js', 'public/sw.js')
+mix.copy('spa/dist/_nuxt', 'public/_nuxt')
+
+mix.copy('spa/dist/index.html', 'resources/views/welcome.blade.php')
