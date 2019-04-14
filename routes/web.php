e8a813e60672b2e81badb17cf83fd6668400332a
Rebecca Scheidegger <rebecca.scheidegger@students.bfh.ch>
2019-04-14 16:07:16 +0200

    Added SPA cors controller

--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+// --------------------- backend ---------------------
+
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::post('/care-provider/store', 'CareProviderController@store');
+Route::group(['middleware' => 'auth'], function() {
 
-Route::get('/affected/view/{id}', 'AffectedController@view');
-Route::get('/affected/items/{id}', 'AffectedController@items');
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::post('/care-provider/store', 'CareProviderController@store');
 
-Route::get('/affected/create', 'AffectedController@create');
-Route::get('/affected/search', 'AffectedController@search');
-Route::post('/affected/store', 'AffectedController@store');
+    Route::get('/affected/view/{id}', 'AffectedController@view');
+    Route::get('/affected/items/{id}', 'AffectedController@items');
 
+    Route::get('/affected/create', 'AffectedController@create');
+    Route::get('/affected/search', 'AffectedController@search');
+    Route::post('/affected/store', 'AffectedController@store');
 
-Route::get('/affectedItems/create', 'AffectedItemsController@create');
-Route::post('/affectedItems/store', 'AffectedItemsController@store');
-Route::get('/affectedItems/delete', 'AffectedItemsController@delete');
+    Route::get('/affectedItems/create', 'AffectedItemsController@create');
+    Route::post('/affectedItems/store', 'AffectedItemsController@store');
+    Route::get('/affectedItems/delete', 'AffectedItemsController@delete');
 
-Route::get('/pdf/{id}', 'PdfController@index');
-Route::get('/pdf/html/{id}', 'PdfController@html');
+    Route::get('/pdf/{id}', 'PdfController@index');
+    Route::get('/pdf/html/{id}', 'PdfController@html');
+
+});
