413c6ef5206b1d325872301c699c4edceef5357e
scher4 <rebecca.scheidegger@students.bfh.ch>
2019-04-19 11:58:00 +0200

    Fix webpack built for SPA assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,29 @@
 const mix = require('laravel-mix');
-require('laravel-mix-copy-watched');
+const Clean = require('clean-webpack-plugin');
+const mixCopyWatched = require('laravel-mix-copy-watched');
+
+mix.webpackConfig({
+    plugins: [
+        new Clean({
+            cleanOnceBeforeBuildPatterns: [
+                'public/_nuxt'
+            ],
+            verbose: true
+        })
+    ],
+});
 
 mix.js('resources/js/app.js', 'public/js')
 mix.sass('resources/sass/app.scss', 'public/css');
 
-// mix.copyWatched('spa/dist/.nojekyll', 'public/.nojekyll')
-// mix.copyWatched('spa/dist/200.html', 'public/200.html')
-// mix.copyWatched('spa/dist/icon.png', 'public/icon.png')
-// mix.copyWatched('spa/dist/sw.js', 'public/sw.js')
-// mix.copyWatched('spa/dist/index.html', 'resources/views/welcome.blade.php')
+mix.copyWatched('spa/dist/.nojekyll', 'public/.nojekyll')
+mix.copyWatched('spa/dist/200.html', 'public/200.html')
+mix.copyWatched('spa/dist/icon.png', 'public/icon.png')
+mix.copyWatched('spa/dist/sw.js', 'public/sw.js')
+mix.copyWatched('spa/dist/index.html', 'resources/views/welcome.blade.php')
 
-// mix.copyWatched(
-//     'spa/dist/_nuxt/**/*',
-//     'public/_nuxt/',
-//     { base: '' }
-// );
+mix.copyWatched('spa/dist/_nuxt/**/*.js(on)', 'public/_nuxt/', { base: '' });
+mix.copyWatched('spa/dist/_nuxt/**/*.js', 'public/_nuxt/', { base: '' });
+mix.copyWatched('spa/dist/_nuxt/**/icons/*',  'public/_nuxt/icons/',  { base: '' });
+
+
