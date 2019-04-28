67cb4e6313d94c93e4df548e56ef4a51fe4bb6f3
Sandra Burri <sandra.burri@students.bfh.ch>
2019-04-28 16:33:06 +0200

    Impressum completed for the affected persons

--- spa/pages/legal/impressum.vue
+++ spa/pages/legal/impressum.vue
@@ -1,6 +1,23 @@
 <template>
     <section class="container-fluid">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, libero repellat ex voluptate praesentium impedit perferendis, nemo iure earum distinctio a, reprehenderit labore laboriosam vitae sunt debitis qui aliquam soluta!</p>
+        <p> Herausgeber der Web Applikation für Leistungserbringer und der Progressiven Web Applikation für Betroffene:<br />
+BFH Berner Fachhochschule<br />
+Technik und Informatik<br />
+Höheweg 80<br />
+2501 Biel<br />
+Im Modul Living Case 2<br />
+Betreuer: Prof. Dr. Christopher Lueg<br />
+Konzeptverantwortliche und Programmierer:<br />
+Sandra Burri, sandra.burri@students.bfh.ch<br />
+Rebecca Scheidegger, rebecca.scheidegger@students.bfh.ch<br />
+<br />
+Auftraggeber:<br />
+<b>aha! Allergiezentrum Schweiz</b><br />
+Scheibenstrasse 20<br />
+Postfach 1<br />
+CH-3000 Bern 22<br />
+<a href="https://www.aha.ch" target="_blank">www.aha.ch</a>
+</p>
     </section>
 </template>
 
