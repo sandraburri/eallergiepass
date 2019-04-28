3f7b77fdea54e00ccfb58820019ea3e5bef9615e
Sandra Burri <sandra.burri@students.bfh.ch>
2019-04-28 16:31:17 +0200

    Disclaimer completed for the affected persons

--- spa/pages/legal/tos.vue
+++ spa/pages/legal/tos.vue
@@ -1,6 +1,22 @@
 <template>
     <section class="container-fluid">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, libero repellat ex voluptate praesentium impedit perferendis, nemo iure earum distinctio a, reprehenderit labore laboriosam vitae sunt debitis qui aliquam soluta!</p>
+        <p><b>Haftungsbeschränkung</b><br />
+Die Progressive Web Applikation www.eallergiepass.ch stellt allgemeine und aktuelle Informationen zu den Themenbereichen «Allergien, Intoleranzen, Ekzeme/Hautreaktionen, Asthma» zur Verfügung. Es ist eine Anleitung über das Vorgehen in Notfallsituationen hinterlegt, welche allgemein für Notfälle gültig und nicht individuell auf die Allergien angepasst ist. Die Web Applikation bietet die Möglichkeit, vermutete oder durch Fachpersonen bestätigte Allergien, Intoleranzen und Unverträglichkeiten zu vermerken.<br />
+<br />
+Die Inhalte der Web Applikation für Leistungserbringer und der Progressiven Web Applikation für Betroffene wurden mit grösstmöglicher Sorgfalt und bestem Gewissen erstellt. Dennoch wird keine Gewähr für die Aktualität, Vollständigkeit und Richtigkeit der bereitgestellten Inhalte übernommen, und jegliche Haftung wird seitens der Betreiber abgelehnt. Das gilt auch für Massnahmen, die auf der Grundlage der zur Verfügung gestellten Informationen ergriffen werden.<br />
+<br />
+<b>Mit Bezug auf die Themenbereiche «Allergien und Intoleranzen» kann die Web Applikation www.eallergiepass.ch in keiner Weise eine ärztliche Konsultation oder eine medizinische Behandlung ersetzen.</b><br />
+<br />
+Alle Rechte sind den Betreibern vorbehalten.<br />
+<br />
+<b>Externe Links</b><br />
+Die Progressive Web Applikation enthält «externe Links» (Verlinkungen) zu anderen Webseiten, auf diese die Betreiber keinen Einfluss haben. Aus diesem Grund kann für diese Inhalte auch keine Gewähr übernommen werden. Für die Inhalte und Richtigkeit der bereitgestellten Informationen ist der jeweilige Anbieter der verlinkten Webseite verantwortlich. Zum Zeitpunkt der Verlinkung waren keine Rechtsverstösse erkennbar. Bei Bekanntwerden einer solchen Rechtsverletzung wird der Link umgehend entfernt.<br />
+<br />
+<b>Unabhängigkeit</b><br />
+Die Progressive Web Applikation und die Web Applikation wurden völlig unabhängig von Sponsoren oder Kooperationspartnern erarbeitet. Diese nehmen auf den Inhalt keinerlei Einfluss.<br />
+<br />
+Stand: April 2019<br />
+</p>
     </section>
 </template>
 
