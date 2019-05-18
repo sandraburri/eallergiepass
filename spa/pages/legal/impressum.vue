28c3c3e87d2214431fb9417de5c2dd7569769f44
Sandra Burri <sandra.burri@students.bfh.ch>
2019-05-18 09:19:11 +0200

    Impressum content corrected

--- spa/pages/legal/impressum.vue
+++ spa/pages/legal/impressum.vue
@@ -1,22 +1,21 @@
 <template>
     <section class="container-fluid">
-        <p> Herausgeber der Web Applikation für Leistungserbringer und der Progressiven Web Applikation für Betroffene:<br />
-BFH Berner Fachhochschule<br />
-Technik und Informatik<br />
-Höheweg 80<br />
-2501 Biel<br />
-Im Modul Living Case 2<br />
-Betreuer: Prof. Dr. Christopher Lueg<br />
+<p> Herausgeber der Web Applikation für Leistungserbringer und der Progressiven Web Applikation für Betroffene:<br />
 Konzeptverantwortliche und Programmierer:<br />
 Sandra Burri, sandra.burri@students.bfh.ch<br />
 Rebecca Scheidegger, rebecca.scheidegger@students.bfh.ch<br />
+Betreuer: Prof. Dr. Christopher Lueg<br />
 <br />
 Auftraggeber:<br />
 <b>aha! Allergiezentrum Schweiz</b><br />
 Scheibenstrasse 20<br />
 Postfach 1<br />
 CH-3000 Bern 22<br />
-<a href="https://www.aha.ch" target="_blank">www.aha.ch</a>
+<a href="https://www.aha.ch" target="_blank">www.aha.ch</a><br />
+<br />
+Allgemeiner Hinweis:<br />
+Ohne ausdrückliche schriftliche Genehmigung von Sandra Burri und Rebecca Scheidegger, darf nichts von eAllergiepass vervielfältigt oder übertragen werden, unabhängig davon, auf welche Art und Weise und mit welchen Mitteln (elektronisch oder mechanisch) dies geschieht.<br />
+Alle Rechte vorbehalten.
 </p>
     </section>
 </template>
